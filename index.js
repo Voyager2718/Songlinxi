ae97793868681a762caaaaf3ce6ea85ebf4716e3
Voyager2718 <voyager2718@gmail.com>
2017-04-19 23:51:28 +0800

    Added container box and blur

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 angular.module('index', ['ngMaterial'])
     .controller('AppCtrl', function ($scope) {
+        $scope.showcard = true; // Fixme: Only For Test
+        $scope.blur = 'none';
+
         $scope.rating = {
             love: 50,
             life: 50,
@@ -12,5 +15,15 @@ angular.module('index', ['ngMaterial'])
                 life: 50,
                 safety: 50,
             };
+        }
+
+        $scope.show = function () {
+            $scope.showcard = true;
+            $scope.blur = 'blur(5px)';
+        }
+
+        $scope.cancel = function () {
+            $scope.showcard = false;
+            $scope.blur = 'none';
         }
     });
